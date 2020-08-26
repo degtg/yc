cf7010c24fc146d2edfc68690fac5458e278fe29
“Youdegtg <3380193683@qq.com>
2020-08-26 15:52:42 +0800

    blog 管理查询分类信息

--- LxBlog/src/main/java/com/yc/LxBlog/dao/ArticleMapper.java
+++ LxBlog/src/main/java/com/yc/LxBlog/dao/ArticleMapper.java
@@ -2,8 +2,13 @@ package com.yc.LxBlog.dao;
 
 import java.util.List;
 
+
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.One;
 import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.ResultMap;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 import com.yc.LxBlog.bean.Article;
@@ -11,9 +16,19 @@ import com.yc.LxBlog.bean.Article;
 public interface ArticleMapper {
 
 	@Select("select * from article order by createtime desc")
+	// <ResultMap> == @Results
+	@Results(id="rmAm",value= {
+			@Result(id = true, column = "id", property = "id"), // 缓存性能优化用到, // 缓存性能优化用到
+			@Result(column = "categoryid", property = "categoryid"), // 缓存性能优化用到
+			@Result(
+					column = "categoryid", 
+					property = "category",
+					one=@One(select="com.yc.LxBlog.dao.CategoryMapper.selectById"))
+	})
 	public List<Article> selectByNew();
 	
 	@Select("select * from article where id=#{id}")
+	@ResultMap("rmAm")
 	public Article selectById(int id);
 
 	@Insert("insert into article values (#{id},#{author},#{title},#{content},"
