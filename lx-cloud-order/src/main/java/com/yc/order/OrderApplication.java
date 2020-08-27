c4f47c3eec6e7d8b19cbd2aa660f2dbd95d7d925
“Youdegtg <3380193683@qq.com>
2020-08-27 10:53:07 +0800

    springcloud 远程服务互访

--- lx-cloud-order/src/main/java/com/yc/order/OrderApplication.java
+++ lx-cloud-order/src/main/java/com/yc/order/OrderApplication.java
@@ -2,6 +2,8 @@ package com.yc.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
@@ -12,6 +14,9 @@ public class OrderApplication implements WebMvcConfigurer{
 		SpringApplication.run(OrderApplication.class, args);
 	}
 
-	
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 
 }
--- lx-cloud-user/src/main/java/com/yc/user/UserApplication.java
+++ lx-cloud-user/src/main/java/com/yc/user/UserApplication.java
@@ -2,6 +2,8 @@ package com.yc.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
@@ -12,6 +14,12 @@ public class UserApplication implements WebMvcConfigurer{
 		SpringApplication.run(UserApplication.class, args);
 	}
 
-	
+	/**
+	 * 	定义 RestTemplate  Bean
+	 */
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
 
 }
