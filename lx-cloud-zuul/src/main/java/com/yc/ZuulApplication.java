d9c30de647effbb7744746d30eb1df4c94872e37
“Youdegtg <3380193683@qq.com>
2020-08-29 16:00:44 +0800

    zuul路由网关配置

---
+++ lx-cloud-zuul/src/main/java/com/yc/ZuulApplication.java
@@ -0,0 +1,31 @@
+package com.yc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+//zuul网关注解
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class ZuulApplication implements WebMvcConfigurer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulApplication.class, args);
+	}
+
+	/**
+	 * 	定义 RestTemplate  Bean
+	 */
+	@LoadBalanced
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+}
